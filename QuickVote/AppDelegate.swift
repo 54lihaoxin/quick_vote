55d44270563eb1b7a2cad694e18a526e92da3297
Haoxin Li <54lihaoxin@gmail.com>
2018-07-16 20:40:36 -0700

    Project init

---
+++ QuickVote/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  QuickVote
+//
+//  Created by Haoxin Li on 7/16/18.
+//  Copyright © 2018 Haoxin Li. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+}
